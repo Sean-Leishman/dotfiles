b5744ed26e6b119b4599da0c9718c4d03e0c01dc
Sean Leishman <s2051283@ed.ac.uk>
2025-08-03 11:11:19 +0100

    Add leetcode nvim and commands to install necessary zsh packages

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/bash
 
 STOWED_FOLDERS=("bash" "nvim" "tmux" "starship" "oh-my-posh" "zsh" "profile")
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
 for item in "${STOWED_FOLDERS[@]}"
 do
@@ -9,3 +12,5 @@ do
     stow -D $item
     stow $item
 done
+
+exec zsh
