c0b8929c4f175f468e99e950c0592a430d54235d
sean-leishman <leishmansean@gmail.com>
2025-09-07 11:21:29 +0100

    Merge .zshrc and remove starship from install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-STOWED_FOLDERS=("bash" "nvim" "tmux" "starship" "oh-my-posh" "zsh" "profile")
+STOWED_FOLDERS=("bash" "nvim" "tmux" "oh-my-posh" "zsh" "profile")
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -135,3 +135,4 @@ POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(anaconda ...ENVS)
 export NVM_DIR="$HOME/.config/nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+eval "$(uv generate-shell-completion zsh)"
